2b1ddf4d506cedde7e61b52ca81407e13c7f4840
Hector Miller-Bakewell <hmillerbakewell@gmail.com>
2021-07-25 10:37:35 +0100

    Update version number in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Hector Miller-Bakewell'
 author = 'Hector Miller-Bakewell'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
